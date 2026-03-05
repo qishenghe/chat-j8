0fde3de1fdbd0202a067752cd80e4890a18e7739
qishenghe <qishenghe>
2026-03-05 16:34:57 +0800

    init

---
+++ src/main/java/com/qishenghe/chatj8/Test.java
@@ -0,0 +1,14 @@
+package com.qishenghe.chatj8;
+
+/**
+ * chat-j8
+ *
+ * @author qishenghe
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+}
